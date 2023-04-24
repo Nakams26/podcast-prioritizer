6f2899ed40c8265dec77b417ff10829ba3028603
adrienpolselli <107076213+Nakams26@users.noreply.github.com>
2023-04-24 18:06:12 -0400

    Adjusting media queries

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,22 +1,18 @@
 //Import router link for rerouting
 import { Link } from "react-router-dom";
-import vinyl from "../assets/vecteezy_vinyl-record-vector-illustration-isolated-on-white-background_9313617_181.png";
 
 const Welcome = () => {
   return (
     <section className="welcomePage">
       <div className="wrapper flexWelcome">
-        <h1>Podcast Prioritizer</h1>
-        {/* <div className="imgWelcome">
-          <img src={vinyl} alt="An image of a vinyl disc" />
-        </div> */}
+        <div><h1>Podcast Prioritizer</h1>
         <p className="textWelcome">
           Are you going somewhere? Let us know your travel, we'll find a podcast
-          for you!
+          for you!</p></div>
           <Link to="/podcast">
-            <span> Click here to start</span>
+            <p> Click here to start</p>
           </Link>
-        </p>
+     
       </div>
     </section>
   );
